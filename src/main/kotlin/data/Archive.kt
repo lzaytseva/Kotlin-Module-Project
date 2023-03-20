16fcb9e23c148b4f3655c0238867babf6251ecd8
ledrabbit <lvzaytseva1@gmail.com>
2023-03-20 10:32:03 +0300

    Archive class changed to data class

--- src/main/kotlin/data/Archive.kt
+++ src/main/kotlin/data/Archive.kt
@@ -1,20 +1,5 @@
 package data
 
-class Archive(val name: String) {
+data class Archive(val name: String) {
     val notes = mutableListOf<Note>()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Archive
-
-        if (name != other.name) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return name.hashCode()
-    }
 }
